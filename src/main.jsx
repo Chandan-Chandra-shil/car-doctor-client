5884960bf06cca26895e2e3f41f6fcc01fc53b17
Chandan-Chandra-shil <chandanchandrashil22@gmail.com>
2023-05-10 01:04:51 +0600

    add simple carousel and gradient color

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,11 @@ import './index.css'
 import { RouterProvider } from "react-router-dom";
 import router from './Routes/Routes.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
- <RouterProvider router={router}/>
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <div className='max-w-7xl mx-auto'>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+    ,
+  </div>
+);
